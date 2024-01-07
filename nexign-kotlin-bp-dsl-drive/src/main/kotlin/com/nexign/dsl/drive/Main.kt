b06fe12c7f3cca690a71d2e98e667ec954e2b3b2
Kirill Kreslavski <kirill.kreslavski@gmail.com>
2024-01-07 19:17:52 +0300

    small code changes and addition of presentation and all of its materials

--- nexign-kotlin-bp-dsl-drive/src/main/kotlin/com/nexign/dsl/drive/Main.kt
+++ nexign-kotlin-bp-dsl-drive/src/main/kotlin/com/nexign/dsl/drive/Main.kt
@@ -1,13 +1,16 @@
 package com.nexign.dsl.drive
 
 import com.nexign.dsl.scenarios.examples.arithmeticscenario.ArithmeticScenario
+import com.nexign.dsl.scenarios.examples.bpscenario.ExampleScenario
+import com.nexign.dsl.scenarios.examples.bpscenario.mock.Abonent
+import com.nexign.dsl.scenarios.examples.bpscenario.mock.Action
 
 
 fun main(args: Array<String>) {
     val scenario = ArithmeticScenario(
         mutableMapOf(
             "a" to 12.0,
-            "b" to 5.5
+            "b" to 5.5,
         )
     )
     scenario.run {  }
@@ -17,4 +20,12 @@ fun main(args: Array<String>) {
     scenario.getLastRun().forEach {
         println(it.description)
     }
+
+    val bpScenario = ExampleScenario(
+        mutableMapOf(
+            "abonent" to Abonent("erf156-15edyu-98wer7"),
+            "action" to Action("quiz"),
+        )
+    )
+    bpScenario.run {  }
 }
--- nexign-kotlin-bp-dsl-scenarios/src/main/kotlin/com/nexign/dsl/scenarios/examples/bpscenario/ExampleScenario.kt
+++ nexign-kotlin-bp-dsl-scenarios/src/main/kotlin/com/nexign/dsl/scenarios/examples/bpscenario/ExampleScenario.kt
@@ -5,23 +5,26 @@ import com.nexign.dsl.base.*
 class ExampleScenario(store: MutableMap<String, Any>) : Scenario(store)  {
 
     override val specification: Specification = specification (
-            GetAbonentInfo() next CheckAbonentActions() binary {
-                yes(ProlongAction() next notifyAboutActionTimePeriod next end)
-                no(ActivateAction() next WriteOffMoney() multiple {
-                    +(YES to (CancelActionActivation() next NotifyAction("error when activating action") next end))
+            getAbonentInfo next checkAbonentActions binary {
+                yes(prolongAction next notifyAboutActionTimePeriod next end)
+                no(activateAction next writeOffMoney multiple {
+                    +(YES to (cancelActionActivation next NotifyAction("error when activating action") next end))
                     +(NO to (NotifyAction("action activation") next notifyAboutActionTimePeriod))
                 })
             }
     )
 
-
     companion object {
+        val getAbonentInfo = GetAbonentInfo()
+        val checkAbonentActions = CheckAbonentActions()
+        val prolongAction = ProlongAction()
+        val activateAction = ActivateAction()
+        val writeOffMoney = WriteOffMoney()
+        val cancelActionActivation = CancelActionActivation()
 
-        // May be should be a global constant
+        val notifyAboutActionTimePeriod = NotifyAction("action time period")
         val end = object : Operation() {
             override val func: Scenario.() -> TransitionCondition = { STOP_EXECUTION }
         }
-
-        val notifyAboutActionTimePeriod = NotifyAction("action time period") next end
     }
 }
